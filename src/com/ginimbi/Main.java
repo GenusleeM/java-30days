8b2e2f094938f9a31c828e17758857ba6819e309
GenusleeM <genuslee.developer@gmail.com>
2023-07-19 21:07:08 +0200

    this is done arrays

--- src/com/ginimbi/Main.java
+++ src/com/ginimbi/Main.java
@@ -1,15 +1,35 @@
 package com.ginimbi;
 
 import java.awt.*;
+import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Ginimbi");
-        Point pointA = new Point(15,10);
-//        System.out.println(pointA);
-        Point pointB = pointA;
-        pointA.x=100;
-        System.out.println(pointA);
-        System.out.println(pointB);
+//Arrays
+        int[] numbers = new int[3];
+        numbers[0]=4;
+        numbers[1]=8;
+        numbers[2]=10;
+
+        System.out.println(Arrays.toString(numbers));
+
+        int[] numbers2 = {0,1,2,3,4,5};
+
+        System.out.println(Arrays.toString(numbers2));
+
+        String [] names = {"Gie", "Lee"};
+        System.out.println(Arrays.toString(names));
+
+//        0 and null values for arrays
+        int[] nums = new int[3];
+        Arrays.fill(nums, 5);
+        nums[2] =25;
+        System.out.println(Arrays.toString(nums));
+
+        String[] name = new String[3];
+        Arrays.fill(name, "foo");
+        name[1] ="Genus lee";
+        System.out.println(Arrays.toString(name));
+
     }
 }

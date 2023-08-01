6bee11e6061a9b3b3fe83c561c51caa4b2a7b652
GenusleeM <genuslee.developer@gmail.com>
2023-08-01 14:57:30 +0200

    done

--- src/com/ginimbi/Main.java
+++ src/com/ginimbi/Main.java
@@ -1,6 +1,7 @@
 package com.ginimbi;
 
 import java.awt.*;
+import java.util.Locale;
 
 public class Main {
     public static void main(String[] args) {
@@ -16,6 +17,13 @@ public class Main {
             default:
                 System.out.println("Zadhakwa");
         }
+
+      String result =  switch (grade){
+            case 'A' -> "Excellent";
+          default ->"Fail";
+        };
+
+        System.out.println(result.toLowerCase(Locale.ROOT));
     }
 
 }

1014fd90eee45792a26c37b28b6ccce98ddffa99
GenusleeM <genuslee.developer@gmail.com>
2023-08-01 14:48:31 +0200

    done with switch

--- src/com/ginimbi/Main.java
+++ src/com/ginimbi/Main.java
@@ -4,37 +4,18 @@ import java.awt.*;
 
 public class Main {
     public static void main(String[] args) {
-//    Loops
-        String[] names = {
-                "Genius",
-                "Genus",
-                "Gin"
-        };
-//        for (int i =0; i< names.length; i++){
-//            System.out.println(names[i]);
-//        };
 
-//        Enhanced for loop
-//        for (String name : names){
-//            System.out.println(name);
-//        }
+//    Switch Statement
 
-//        While Loop
-//        int i = 0;
-//        boolean keepLooping = true;
-//        while (keepLooping){
-//            System.out.println("hello" + i++);
-//            keepLooping=false;
-//        }
+        char grade = 'A';
 
-//        Do while loop
-        boolean keepLooping = true;
-
-        do {
-            System.out.println("Ginimbi");
-            keepLooping= false;
-        }while (keepLooping);
+        switch (grade){
+            case 'A':
+                System.out.println("Ginimbi");
+                break;
+            default:
+                System.out.println("Zadhakwa");
+        }
     }
-
 
 }

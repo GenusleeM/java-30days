d3ce0d20c78df060f60929ef1829734d737b39f2
GenusleeM <genuslee.developer@gmail.com>
2023-08-01 14:32:48 +0200

    enhanced for loop

--- src/com/ginimbi/Main.java
+++ src/com/ginimbi/Main.java
@@ -4,12 +4,21 @@ import java.awt.*;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Ginimbi");
-        Point pointA = new Point(15,10);
-//        System.out.println(pointA);
-        Point pointB = pointA;
-        pointA.x=100;
-        System.out.println(pointA);
-        System.out.println(pointB);
+//    Loops
+        String[] names = {
+                "Genius",
+                "Genus",
+                "Gin"
+        };
+//        for (int i =0; i< names.length; i++){
+//            System.out.println(names[i]);
+//        };
+
+//        Enhanced for loop
+        for (String name : names){
+            System.out.println(name);
+        }
     }
+
+
 }

8b8705fc6a21074be37f232af40c567ec5d333b3
GenusleeM <genuslee.developer@gmail.com>
2023-08-09 11:27:18 +0200

    this is for strings

--- src/com/ginimbi/Fool.java
+++ src/com/ginimbi/Fool.java
@@ -4,11 +4,41 @@ public class Fool {
     public static void main(String[] args) {
 //        System.out.println(Main.foo);
         int result = add(5, 6);
-        System.out.println(result);
+//        System.out.println(result);
+
+        String name = "Genuslee";
+//        System.out.println(name.substring(1));
+//        System.out.println(name.replace('e', 'i'));
+
+//        String Litteral vs String Object
+        String name1 = "Ginimbi";
+        String name22 = "Ginimbi";
+
+        String name2 = new String("Ginimbi");
+
+//        Comparing Strings
+//        System.out.println(name1 == name2);
+//        System.out.println(name1==name22);
+//        System.out.println(name1.equals(name2));
+
+
+//       String num = String.valueOf(1);
+//        System.out.println(num);
+//     String format =   String.format("Number %s", num);
+//        System.out.println(format);
+//        String [] names = {"Ginimbi", "Lloyd"};
+//     String join =   String.join(" | ", names);
+//        System.out.println(join);
+
     };
  private static  int add(int a, int b){
      int result = a + b;
         return result;
     };
+
+
+// Strings
+
+
 
 }

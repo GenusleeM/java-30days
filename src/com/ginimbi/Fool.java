3f3dac4ec1388e13fa8b49ee2e5e6d303d6ac9cd
GenusleeM <genuslee.developer@gmail.com>
2023-08-09 10:44:27 +0200

    this is done

---
+++ src/com/ginimbi/Fool.java
@@ -0,0 +1,14 @@
+package com.ginimbi;
+
+public class Fool {
+    public static void main(String[] args) {
+//        System.out.println(Main.foo);
+        int result = add(5, 6);
+        System.out.println(result);
+    };
+ private static  int add(int a, int b){
+     int result = a + b;
+        return result;
+    };
+
+}
--- src/com/ginimbi/Main.java
+++ src/com/ginimbi/Main.java
@@ -4,26 +4,10 @@ import java.awt.*;
 import java.util.Locale;
 
 public class Main {
+    public  static  String foo = "bar";
     public static void main(String[] args) {
 
-//    Switch Statement
 
-        char grade = 'A';
-
-        switch (grade){
-            case 'A':
-                System.out.println("Ginimbi");
-                break;
-            default:
-                System.out.println("Zadhakwa");
-        }
-
-      String result =  switch (grade){
-            case 'A' -> "Excellent";
-          default ->"Fail";
-        };
-
-        System.out.println(result.toLowerCase(Locale.ROOT));
     }
 
 }

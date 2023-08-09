f6a9a56c2b8da2bdbe0d52ecd258fa0b8b7df831
GenusleeM <genuslee.developer@gmail.com>
2023-08-09 11:41:52 +0200

    this is for localdate

--- src/com/ginimbi/Fool.java
+++ src/com/ginimbi/Fool.java
@@ -1,5 +1,10 @@
 package com.ginimbi;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.util.Locale;
+
 public class Fool {
     public static void main(String[] args) {
 //        System.out.println(Main.foo);
@@ -30,6 +35,23 @@ public class Fool {
 //     String join =   String.join(" | ", names);
 //        System.out.println(join);
 
+
+//        LocalDate
+      LocalDateTime now =  LocalDateTime.now();
+        System.out.println(now);
+        System.out.println(now.getMonth());
+        System.out.println(now.getDayOfWeek());
+        System.out.println(now.getDayOfYear());
+        System.out.println(now.getDayOfMonth());
+        System.out.println(now.getSecond());
+        System.out.println(now.minusDays(5));
+
+//        Localdate
+        LocalDate date = LocalDate.now();
+
+        LocalTime localTime = LocalTime.now();
+        System.out.println(localTime);
+        System.out.println(localTime.isBefore(LocalTime.MIDNIGHT));
     };
  private static  int add(int a, int b){
      int result = a + b;

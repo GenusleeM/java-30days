96175a61a0806b8a20e9d4583eb4c96ac072d38d
GenusleeM <genuslee.developer@gmail.com>
2023-08-10 12:15:02 +0200

    ojos

--- src/com/ginimbi/Car.java
+++ src/com/ginimbi/Car.java
@@ -1,2 +1,21 @@
-package com.ginimbi;public class Car {
+package com.ginimbi;
+
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+
+
+public class Car implements Serializable {
+
+    //Java bean
+//A class must have noarg constructor
+// A class fields must be private and can be accessed thru getters and setters
+// A class should implement serializable
+    private String regNumber;
+    private BigDecimal price;
+
+    public Car(){}
+
+
 }
+
